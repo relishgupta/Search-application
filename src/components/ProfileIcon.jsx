f5b40e97f06093f05e863eb6aee59e66fa79de03
relish <guptarelish333@gmail.com>
2023-06-19 16:43:29 +0530

    started developing homepage

--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -1,10 +1,18 @@
 import { TbGridDots } from "react-icons/tb";
 
 import Profile from "../assets/profile-200x200.jpg";
-import ProfileRing from "../assets/profile-ring.svg";
 
 const ProfileIcon = () => {
-    return <div>Profile Icon</div>;
+    return (
+        <div className="flex gap-2">
+            <span className="h-10 w-10 flex items-center justify-center rounded-full cursor-pointer hover:bg-black/[0.05]">
+                <TbGridDots size={20} color="#5f6368" />
+            </span>
+            <span className="h-10 w-10 flex justify-center items-center cursor-pointer">
+                    <img className="rounded-full h-8 w-8" src={Profile} alt="" />
+            </span>
+        </div>
+    )
 };
 
 export default ProfileIcon;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        c: "0 1px 6px rgb(32 33 36 / 28%)",
+        c2: "0 1px 1px rgb(0 0 0 / 10%)",
+    },
+    },
   },
   plugins: [],
 }
